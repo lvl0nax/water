04c594379fb8b63744864b132c4a019daaecf394
Dmitry Gautsel <lvl0nax@gmail.com>
2012-12-02 11:12:31 +0400

    migration for tree

---
+++ db/migrate/20121129195453_add_tree_to_infopage.rb
@@ -0,0 +1,11 @@
+class AddTreeToInfopage < ActiveRecord::Migration
+  def change
+		add_column :infopages, :lft, :integer
+
+  	add_column :infopages, :rgt, :integer
+
+    add_column :infopages, :parent_id, :integer
+
+    add_column :infopages, :depth, :integer
+  end
+end
