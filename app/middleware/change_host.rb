4d9c689c68058fd1829757abaae486b2967f6b28
lvl0nax <lvl0nax@gmail.com>
2014-08-19 22:17:46 +0400

    russian

--- app/middleware/change_host.rb
+++ app/middleware/change_host.rb
@@ -8,6 +8,8 @@ class ChangeHost
   def call(env)
     if env['HTTP_HOST'].to_s.include?('прима-аква.рф')
       [301, { 'Location' => Rack::Request.new(env).url.sub('прима-аква.рф', 'prima-aqua.ru') }, ['Redirecting...']]
+    elsif env['HTTP_HOST'].to_s.include?('xn----7sbbaj6armzk.xn--p1ai')
+      [301, { 'Location' => Rack::Request.new(env).url.sub('xn----7sbbaj6armzk.xn--p1ai', 'prima-aqua.ru') }, ['Redirecting...']]
     else
       @app.call(env)
     end
